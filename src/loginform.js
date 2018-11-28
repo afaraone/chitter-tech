d3554214c8d6829095ab310ce58cc9a38e21f14f
afaraone <andresfaraone@outlook.com>
2018-11-28 18:16:56 +0000

    Login form has input and callback for submit

--- src/loginform.js
+++ src/loginform.js
@@ -1,7 +1,35 @@
 import React, {Component} from 'react'
 
 class LoginForm extends Component {
-	render(){}
+	constructor(props) {
+		super(props)
+		this.state = { handle: '' }
+		this.updateHandle = this.updateHandle.bind(this)
+		this.updatePassword = this.updatePassword.bind(this)
+	}
+
+	updateHandle(input) {
+		this.setState({
+			handle: input.target.value
+		})
+	}
+
+	updatePassword(input) {
+		this.setState({
+			password: input.target.value
+		})
+	}
+
+	render() {
+		let {handle, password} = this.state
+		return (
+			<div className='login-form-container'>
+			<input type='text' className='login-form' id='login-handle' onChange={this.updateHandle}/>
+			<input type='text' className='login-form' id='login-password' onChange={this.updatePassword}/>
+			<button id='login-submit' onClick={() => this.props.postLogin(handle, password)}>Submit</button>
+			</div>
+		)
+	}
 }
 
 export default LoginForm
--- src/loginform.test.js
+++ src/loginform.test.js
@@ -2,8 +2,38 @@ import React from 'react';
 import { shallow, mount } from 'enzyme';
 import LoginForm from './loginform';
 
+let postLogin = jest.fn()
+
 describe('LoginForm', () => {
-	it('renders without crashing', () => {
-		shallow(<LoginForm />);
+	let wrapper
+
+	beforeEach(() => {
+		wrapper = shallow(<LoginForm postLogin={postLogin}/>)
+	})
+
+	it('renders a form', () => {
+		let loginHandle = <input type='text' className='login-form' id='login-handle'/>
+		let loginPassword = <input type='text' className='login-form' id='login-password'/>
+		let submitButton = <button>Submit</button>
+		expect(wrapper.containsMatchingElement(loginHandle)).toEqual(true)
+		expect(wrapper.containsMatchingElement(loginPassword)).toEqual(true)
+		expect(wrapper.containsMatchingElement(submitButton)).toEqual(true)
+	})
+
+	it('state.handle is set to val in input box', () => {
+		let loginHandle = wrapper.find('#login-handle')
+		loginHandle.simulate('change', {target: {value: 'Test'}})
+		expect(wrapper.state('handle')).toEqual('Test')
+	})
+
+	it('state.handle is set to val in input box', () => {
+		let loginPassword = wrapper.find('#login-password')
+		loginPassword.simulate('change', {target: {value: 'Test'}})
+		expect(wrapper.state('password')).toEqual('Test')
+	})
+
+	it('submit triggers props callback', () => {
+		wrapper.find('#login-submit').simulate('click')
+		expect(postLogin).toHaveBeenCalled()
 	})
 })
