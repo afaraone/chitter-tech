5078ae1cc9b0e75b9fc2a810a2e89958b7f251d7
afaraone <andresfaraone@outlook.com>
2018-11-29 22:33:55 +0000

    Peep renders body and author

--- src/peep.js
+++ src/peep.js
@@ -2,7 +2,13 @@ import React, {Component} from 'react';
 
 class Peep extends Component {
   render() {
-    return(<></>)
+    const {body, created_at, user } = this.props.data
+    return(
+      <div>
+        <h2 className='peep-handle'>{user.handle}</h2>
+        <h2 className='peep-body'>{body}</h2>
+      </div>
+    )
   }
 }
 
---
+++ src/peep.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import Peep from './peep'
+
+let peepData = {"id": 3, "body": "my first peep :)", "created_at": "2018-06-23T13:21:23.317Z",
+"updated_at": "2018-06-23T13:21:23.317Z", "user": {"id": 1, "handle": "kay"},
+"likes": [{"user": {"id": 1, "handle": "kay"}}]}
+
+describe('Peep', () => {
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = shallow(<Peep data={peepData} key={peepData.id}/>)
+  })
+
+  it('renders user handle', () => {
+    let handle = <h2 className='peep-handle'>kay</h2>
+    expect(wrapper.containsMatchingElement(handle)).toEqual(true)
+  })
+
+  it('renders a body', () => {
+    let body = <h2 className='peep-body'>my first peep :)</h2>
+    expect(wrapper.contains(body)).toEqual(true)
+  })
+})
